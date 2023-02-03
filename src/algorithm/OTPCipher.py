6f1418d6efc1eece9b7f407a6ad4ed4df0017a49
Riftkey <rifqimatondang@gmail.com>
2023-02-03 22:55:11 +0700

    Bugfixed OTP plaintext

--- src/algorithm/OTPCipher.py
+++ src/algorithm/OTPCipher.py
@@ -59,11 +59,13 @@ def EnkripsiOTP(plaintext) :
     otpkey= read_otpkey(counter)
     encrypt = vigenere_cipher(plaintext,otpkey,operation)
     
+    
     return encrypt
 
 def OTPkey(plaintext) :
     counter = wordCounter(plaintext)
     otpkey = read_otpkey(counter)
+    deleteUsedKey(counter)
     
     return otpkey
 
@@ -72,45 +74,25 @@ def DekripsiOTP(ciphertext, otpkey) :
     key = otpkey
     decrypt = vigenere_cipher(ciphertext,key,operation)
     return decrypt
-        
-def encrypt_file(file_path):
-    with open(file_path, "r") as file:
-        plaintext = file.read()
-        counter = wordCounter(plaintext)
-    otpkey = read_otpkey(counter)
-    print('Berikut adalah OTP Key yang digunakan: ' + otpkey )
-    ciphertext = vigenere_cipher(plaintext, otpkey,'encrypt')
-    with open(file_path, "w") as file:
-        file.write(ciphertext)
-
-def decrypt_file(file_path):
-    """Decrypts the contents of the file at file_path using the one-time pad cipher"""
-    key = input('Masukkan Key: ')
-    with open(file_path, "r") as file:
-        ciphertext = file.read()
-    plaintext = vigenere_cipher(ciphertext, key,'decrypt')
-    with open(file_path, "w") as file:
-        file.write(plaintext)
 
 def read_otpkey(counter):
     otpkey=''
-    with open('src\otpkey.txt') as f:
+    with open('otpkey.txt') as f:
         for i in range (0,counter):
             lines = f.read(1)
             otpkey = otpkey+lines
 
-    deleteUsedKey(counter)
     f.close()
     return otpkey
 
 def deleteUsedKey(counter):
-    with open("src\otpkey.txt", "r") as file:
+    with open("otpkey.txt", "r") as file:
         content = file.read()
 
     # Remove the first counter characters from the string
     content = content[counter:]
 
-    with open("src\otpkey.txt", "w") as file:
+    with open("otpkey.txt", "w") as file:
         file.write(content)
 
 def read_from_file(randomfile):
